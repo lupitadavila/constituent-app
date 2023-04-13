701ed46da78fca0a3a4d2e7a0a42e63eecd68b3e
Lupita Davila <lupita.dav@gmail.com>
2023-04-12 22:28:21 -0700

    update typescript error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,9 +6,12 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  // @ts-expect-error
   if (!global.prisma) {
+    // @ts-expect-error
     global.prisma = new PrismaClient();
   }
+  // @ts-expect-error
   prisma = global.prisma;
 }
 
